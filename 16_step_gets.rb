654adf09197701e2a13983289cf65aa0dcb9514f
romigod <bolistovskiyroman@gmail.com>
2020-12-31 18:48:36 +0200

    16 step

---
+++ 16_step_gets.rb
@@ -0,0 +1,9 @@
+puts 'Введите число 1:'
+var1 = gets.chomp.to_i
+puts 'Введите число 2:'
+var2 = gets.chomp.to_i
+puts 'Введите число 3:'
+var3 = gets.chomp.to_i
+res = (var1 + var2 + var3) / 3
+puts "Среднее арифметическое: #{res}"
+
