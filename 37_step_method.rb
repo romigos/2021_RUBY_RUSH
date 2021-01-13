b5cb7a006c6ed7a3d2a3a45a8b1115b449a80cf4
romigod <bolistovskiyroman@gmail.com>
2021-01-13 22:40:09 +0200

    37 step

---
+++ 37_step_method.rb
@@ -0,0 +1,13 @@
+puts 'Сколько вам звезд на погоны?'
+
+def shoting_star(star)
+  cicle = 0
+  while cicle < star
+    print "*"
+    cicle += 1
+  end
+end
+
+star = gets.chomp.to_i
+puts 'Вот ваши звезды : '
+print shoting_star(star)
