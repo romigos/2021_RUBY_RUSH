e73102b37446398177cd713bea537eedb04cf68e
romigos <bolistovskiyroman@gmail.com>
2021-01-13 14:51:39 +0200

    32 step

--- 32_step_loop.rb
+++ 32_step_loop.rb
@@ -4,8 +4,16 @@ user_choice = gets.chomp.to_i
 puts 'Какое время года? (0 - весна, 1 - лето, 2 - осень, 3 - зима)'
 time_choice = gets.chomp.to_i
 
-if user_choice >= 15 && user_choice <= 35 && time_choice == 1
-  puts 'Скорее идите в парк, соловьи поют!'
+if time_choice == 1
+  if user_choice >= 15 && user_choice <= 35
+    puts 'Скорее идите в парк, соловьи поют!'
+  else
+    puts 'Сейчас соловьи молчат,'
+  end
 else
-  puts 'Не та пора'
+  if user_choice >= 22 && user_choice <= 30
+    puts 'Скорее идите в парк, соловьи поют!'
+  else
+    puts 'Сейчас соловьи молчат,'
+  end
 end
