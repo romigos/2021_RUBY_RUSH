dd943d38b37eeb4a64632b676e2fb8a8ecad600d
romigos <bolistovskiyroman@gmail.com>
2021-01-16 15:03:04 +0200

    46 step

--- 46_step_class_person.rb
+++ 46_step_class_person.rb
@@ -1,10 +1,25 @@
 class Person
-  def initialize(first_name, middle_name)
+  def initialize(first_name, middle_name, age)
     @first_name = first_name
     @middle_name = middle_name
+    @age = age
   end
 
   def full_name
-    "#{@first_name} #{@middle_name}"
+    if @age >= 60
+      "#{@first_name} #{@middle_name}"
+    else
+      "#{@first_name}"
+    end
   end
+
+  def old
+    if @age >= 60
+      "He is #{@age}" " - old"
+    else
+      "He is #{@age}" " - young"
+    end
+
+  end
+
 end
--- 46_step_class_person_main.rb
+++ 46_step_class_person_main.rb
@@ -1,8 +1,11 @@
 require './46_step_class_person.rb'
-person1 = Person.new('Гаврила', 'Петрович')
-person2 = Person.new('Фёдор', 'Петрович')
-person3 = Person.new('Василий', 'Алибабаевич')
+person1 = Person.new('Гаврила', 'Петрович', 64)
+person2 = Person.new('Фёдор', 'Петрович', 18)
+person3 = Person.new('Василий', 'Алибабаевич', 60)
 puts 'У нас есть три человека:'
 puts person1.full_name
+puts person1.old
 puts person2.full_name
+puts person2.old
 puts person3.full_name
+puts person3.old
